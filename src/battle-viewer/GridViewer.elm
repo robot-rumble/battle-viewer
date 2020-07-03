f745a553a7ea384a71b4f6227be43d10e1c9aef5
Anton <antonoutkine@gmail.com>
2020-07-02 21:22:30 -0500

    Open debug docs in new tab

--- src/battle-viewer/GridViewer.elm
+++ src/battle-viewer/GridViewer.elm
@@ -249,7 +249,7 @@ viewRobotInspector model unitId =
                                         Dict.toList robotOutput.debugTable
                                   in
                                   if List.isEmpty debugPairs then
-                                    p [ class "info" ] [ text "no watch data. ", a [ href "https://rr-docs.readthedocs.io/en/latest/quickstart.html#debugging-your-robot" ] [ text "learn more" ] ]
+                                    p [ class "info" ] [ text "no watch data. ", a [ href "https://rr-docs.readthedocs.io/en/latest/quickstart.html#debugging-your-robot", target "_blank" ] [ text "learn more" ] ]
 
                                   else
                                     div [ class "_table" ] <|

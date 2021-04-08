21d74c365cd6de8fedc1cc0edb0ac26f615b5c93
Anton <antonoutkine@gmail.com>
2021-04-07 23:59:05 -0500

    Load sentry from the Garage

--- src/garage/app.js
+++ src/garage/app.js
@@ -6,6 +6,21 @@ import * as Comlink from 'comlink'
 
 import Split from 'split.js'
 
+import * as Sentry from '@sentry/browser'
+import { Integrations } from '@sentry/tracing'
+
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
+
+
 function loadSettings() {
   let settings
   try {
